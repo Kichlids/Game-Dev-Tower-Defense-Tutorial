8e705084b0c9d583bc6e800963cf99e70c4cdd6c
Kichlids <kichangsongks@gmail.com>
2020-10-20 18:43:17 -0500

    Navigation and basic enemy movement

---
+++ Unity Tutorial/Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Navigation navigation;
+
+    public Transform[] waypoints;
+    public int waypointIndex = 0;
+
+    public float speed = 0.5f;
+
+    public float distThreshold = 0.01f;
+
+    private void Start() {
+        waypoints = navigation.waypoints;
+    }
+
+    private void Update() {
+
+        if (waypointIndex >= waypoints.Length) {
+            Destroy(gameObject);
+            enabled = false;
+            return;
+        }
+
+        float distance = Vector3.Distance(transform.position, waypoints[waypointIndex].position);
+
+        if (distance < distThreshold) {
+            // Go to next waypoint
+            waypointIndex++;
+        }
+        else {
+            print(waypoints[waypointIndex].position);
+            transform.Translate((waypoints[waypointIndex].position - transform.position).normalized * speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Unity Tutorial/Assets/Scripts/Move.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Transform cubeTransform;
+
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubeTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move with WASD
+
+        Vector3 direction = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W)) {
+            direction.z = 1;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            direction.x = -1;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            direction.z = -1;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            direction.x = 1;
+        }
+
+        cubeTransform.Translate(direction.normalized * speed);
+    }
+}
